ead091d3f626f9ff5be523d76c0c243825bd2dda
juleshb <habaruremajules@gmail.com>
2024-03-16 12:08:55 +0200

    changing api url

--- src/Audiologiste/index.js
+++ src/Audiologiste/index.js
@@ -11,7 +11,7 @@ export default function Admin() {
 
         <div className="relative md:pt-32 pb-32 pt-10"> 
       </div>
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="px-4 md:px-10 mx-auto w-full m-2">
           
           <Routes>
           
--- src/Nurse/uppdateborn.js
+++ src/Nurse/uppdateborn.js
@@ -14,7 +14,7 @@ const nurseViewsinglebon = () => {
 
         <div className="relative md:pt-32 pb-32 pt-10"> 
       </div>
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="px-4 md:px-10 mx-auto w-full m-2">
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6  border border-primary">
 
       <Viewsingle />
